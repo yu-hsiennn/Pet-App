491b18acdd7e796c4c4dce8b93c8c16151503a73
felixwhite <klassemax@gmail.com>
2023-04-30 17:15:49 +0800

    welcome page more adaptable, removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'EditPersonData.dart';
 import 'EditPetData1.dart';
 
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Pet Match',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -31,43 +33,50 @@ class WelcomePage extends StatefulWidget {
 }
 
 class _WelcomePageState extends State<WelcomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Container(
-              margin: EdgeInsets.all(10),
-              alignment: Alignment.topLeft,
-              child: Text('PET MATCH',
-                style: TextStyle(
-                  fontSize: 50,
-                )
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.all(10),
-              alignment: Alignment.topLeft,
-              child: Text('Welcome Page',
-                style: TextStyle(
-                  fontSize: 50,
-                  fontWeight: FontWeight.bold,
-                  
-                )
-              ),
-            ),
-            SizedBox(
-              height: 375,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            Expanded(
+                child: ListView(
               children: [
-                LoginButton(),
-                RegisterButton(),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  alignment: Alignment.topCenter,
+                  child: Text('PET MATCH',
+                      style: TextStyle(
+                        fontSize: 50,
+                      )),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  alignment: Alignment.topCenter,
+                  child: Text('Welcome Page',
+                      style: TextStyle(
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                      )),
+                ),
               ],
+            )),
+            Container(
+              alignment: Alignment.bottomCenter,
+              margin: EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: LoginButton(),
+                    flex: 1,
+                  ),
+                  Expanded(
+                    child: RegisterButton(),
+                    flex: 1,
+                  )
+                ],
+              ),
             ),
           ],
         ),
@@ -89,92 +98,92 @@ class _RegisterPage extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                margin: EdgeInsets.all(10),
-                child: Text("註冊",
-                  style: TextStyle(
-                    fontSize: 45,
-                  ),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.all(10),
-                child: TextFormField(
-                  cursorColor: Theme.of(context).primaryColor,
-                  initialValue: '',
-                  maxLength: 20,
-                  decoration: InputDecoration(
-                    icon: Icon(Icons.account_circle),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xFF6200EE)),
-                    ),
-                    border: OutlineInputBorder(),
-                    hintText: "email",
-                    labelText: 'email',
-                    helperText: 'xxx@gmail.com',
-                    suffixIcon: Icon(
-                      Icons.check_circle,
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.all(10),
-                child: TextFormField(
-                  cursorColor: Theme.of(context).hintColor,
-                  initialValue: '',
-                  maxLength: 20,
-                  obscureText: hidePassword,
-                  decoration: InputDecoration(
-                    icon: Icon(Icons.lock),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xFF6200EE)),
-                    ),
-                    border: OutlineInputBorder(),
-                    hintText: "password",
-                    labelText: 'password',
-                    helperText: 'Helper text',
-                    // errorText: 'Error message',
-                    // suffixIcon: Icon(
-                    //   Icons.error,
-                    // ),
-                    suffixIcon: IconButton(
-                      icon: hidePassword ? Icon(Icons.visibility_off) : Icon(Icons.visibility),
-                      onPressed: () {
-                        setState(() {
-                          hidePassword = !hidePassword;
-                        });
-                      },
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 300,
-              ),
-            ],
-        ),
-        
-
-        SizedBox(
-        width: 550, // set this
-        child:Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
+        body: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            CancelButton(),
-            RegisterConfirmButton(),
+            Container(
+              margin: EdgeInsets.all(10),
+              child: Text(
+                "註冊",
+                style: TextStyle(
+                  fontSize: 45,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              child: TextFormField(
+                cursorColor: Theme.of(context).primaryColor,
+                initialValue: '',
+                maxLength: 20,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.account_circle),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF6200EE)),
+                  ),
+                  border: OutlineInputBorder(),
+                  hintText: "email",
+                  labelText: 'email',
+                  helperText: 'xxx@gmail.com',
+                  suffixIcon: Icon(
+                    Icons.check_circle,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              child: TextFormField(
+                cursorColor: Theme.of(context).hintColor,
+                initialValue: '',
+                maxLength: 20,
+                obscureText: hidePassword,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.lock),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF6200EE)),
+                  ),
+                  border: OutlineInputBorder(),
+                  hintText: "password",
+                  labelText: 'password',
+                  helperText: 'Helper text',
+                  // errorText: 'Error message',
+                  // suffixIcon: Icon(
+                  //   Icons.error,
+                  // ),
+                  suffixIcon: IconButton(
+                    icon: hidePassword
+                        ? Icon(Icons.visibility_off)
+                        : Icon(Icons.visibility),
+                    onPressed: () {
+                      setState(() {
+                        hidePassword = !hidePassword;
+                      });
+                    },
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 300,
+            ),
           ],
         ),
-      )
-        ],
-      )
-    );
+        SizedBox(
+          width: 550, // set this
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CancelButton(),
+              RegisterConfirmButton(),
+            ],
+          ),
+        )
+      ],
+    ));
   }
 }
 
@@ -190,35 +199,33 @@ class RegisterConfirmButton extends StatelessWidget {
       height: 60,
       width: 250,
       decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromRGBO(255, 143, 158, 1),
-            Color.fromRGBO(255, 188, 143, 1),
-          ],
-          begin: Alignment.centerLeft,
-          end: Alignment.centerRight,
-        ),
-        borderRadius: const BorderRadius.all(
-          Radius.circular(25.0),
-        ),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.pink.withOpacity(0.2),
-            spreadRadius: 4,
-            blurRadius: 10,
-            offset: Offset(0, 3),
-          )
-        ]
-      ),
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(255, 143, 158, 1),
+              Color.fromRGBO(255, 188, 143, 1),
+            ],
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+          ),
+          borderRadius: const BorderRadius.all(
+            Radius.circular(25.0),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.pink.withOpacity(0.2),
+              spreadRadius: 4,
+              blurRadius: 10,
+              offset: Offset(0, 3),
+            )
+          ]),
       child: Center(
         child: GestureDetector(
           onTap: () {
             Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => EditPersonDataPage(title: "Hello world!")
-              )
-            );
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        EditPersonDataPage(title: "Hello world!")));
           },
           child: Text(
             '確認',
@@ -249,26 +256,25 @@ class CancelButton extends StatelessWidget {
       height: 60,
       width: 250,
       decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromRGBO(143, 201, 255, 1),
-            Color.fromRGBO(234, 255, 143, 1),
-          ],
-          begin: Alignment.centerLeft,
-          end: Alignment.centerRight,
-        ),
-        borderRadius: const BorderRadius.all(
-          Radius.circular(25.0),
-        ),
-        boxShadow: [
-          BoxShadow(
-            color: Color.fromARGB(255, 33, 96, 243).withOpacity(0.2),
-            spreadRadius: 4,
-            blurRadius: 10,
-            offset: Offset(0, 3),
-          )
-        ]
-      ),
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(143, 201, 255, 1),
+              Color.fromRGBO(234, 255, 143, 1),
+            ],
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+          ),
+          borderRadius: const BorderRadius.all(
+            Radius.circular(25.0),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromARGB(255, 33, 96, 243).withOpacity(0.2),
+              spreadRadius: 4,
+              blurRadius: 10,
+              offset: Offset(0, 3),
+            )
+          ]),
       child: Center(
         child: GestureDetector(
           onTap: () {
@@ -301,41 +307,35 @@ class RegisterButton extends StatelessWidget {
     return Container(
       margin: EdgeInsets.all(5),
       height: 60,
-      width: 250,
       decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromRGBO(255, 143, 158, 1),
-            Color.fromRGBO(255, 188, 143, 1),
-          ],
-          begin: Alignment.centerLeft,
-          end: Alignment.centerRight,
-        ),
-        borderRadius: const BorderRadius.all(
-          Radius.circular(25.0),
-        ),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.pink.withOpacity(0.2),
-            spreadRadius: 4,
-            blurRadius: 10,
-            offset: Offset(0, 3),
-          )
-        ]
-      ),
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(255, 143, 158, 1),
+              Color.fromRGBO(255, 188, 143, 1),
+            ],
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+          ),
+          borderRadius: const BorderRadius.all(
+            Radius.circular(25.0),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.pink.withOpacity(0.2),
+              spreadRadius: 4,
+              blurRadius: 10,
+              offset: Offset(0, 3),
+            )
+          ]),
       child: Center(
         child: GestureDetector(
           onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => RegisterPage()
-              )
-            );
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => RegisterPage()));
           },
           child: Text(
             'Create Account',
-            textAlign: TextAlign.left,
+            textAlign: TextAlign.center,
             style: TextStyle(
               fontFamily: "Netflix",
               fontWeight: FontWeight.w600,
@@ -360,34 +360,32 @@ class LoginButton extends StatelessWidget {
     return Container(
       margin: EdgeInsets.all(5), // padding edge
       height: 60,
-      width: 250,
       decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromRGBO(255, 143, 158, 1),
-            Color.fromRGBO(255, 188, 143, 1),
-          ],
-          begin: Alignment.centerLeft,
-          end: Alignment.centerRight,
-        ),
-        borderRadius: const BorderRadius.all(
-          Radius.circular(25.0),
-        ),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.pink.withOpacity(0.2),
-            spreadRadius: 4,
-            blurRadius: 10,
-            offset: Offset(0, 3),
-          )
-        ]
-      ),
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(255, 143, 158, 1),
+              Color.fromRGBO(255, 188, 143, 1),
+            ],
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+          ),
+          borderRadius: const BorderRadius.all(
+            Radius.circular(25.0),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.pink.withOpacity(0.2),
+              spreadRadius: 4,
+              blurRadius: 10,
+              offset: Offset(0, 3),
+            )
+          ]),
       child: Center(
         child: GestureDetector(
           onTap: () {},
           child: Text(
             'Login',
-            textAlign: TextAlign.left,
+            textAlign: TextAlign.center,
             style: TextStyle(
               fontFamily: "Netflix",
               fontWeight: FontWeight.w600,
@@ -401,4 +399,3 @@ class LoginButton extends StatelessWidget {
     );
   }
 }
-
