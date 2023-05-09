c2239d15a3bd67327825967c00abb9d233ab7317
tears <albert88824@gmail.com>
2023-05-09 19:17:36 +0800

    add storypage

--- lib/MainPage.dart
+++ lib/MainPage.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'UserData.dart';
 import 'ProfilePage.dart';
 import 'ChatOverviewPage.dart';
-import 'HomePage.dart';
+import 'StoryPage.dart';
+import 'PostClass.dart';
 
 //demo user with two pets
 PetDetail demoPet1 = PetDetail(
@@ -20,6 +21,74 @@ UserData demoUser1 = UserData(
   photo: "assets/image/peach.jpg",
   petdatas: [demoPet1, demoPet2],
 );
+
+List<Post> _posts = [
+  Post(
+      poster: _users[0],
+      post_info: "Hi! First Post!!",
+      pictures: "assets/image/dog1.jpg",
+      label: ['可愛', '調皮'],
+      like_count: 251234,
+      comments: [_comment[1], _comment[2], _comment[3], _comment[4]]),
+  Post(
+      poster: _users[2],
+      post_info: "Hi! Nice to meet all!!",
+      pictures: "assets/image/dog4.jpg",
+      label: ['好吃', '美味'],
+      like_count: 34,
+      comments: [
+        _comment[1],
+        _comment[0],
+      ]),
+  Post(
+      poster: _users[4],
+      post_info: "So hot!!",
+      pictures: "assets/image/dog5.jpg",
+      label: ['不乖就是備用糧食'],
+      like_count: 251,
+      comments: [_comment[5], _comment[6]]),
+];
+
+List<UserData> _users = [
+  UserData(
+      name: "Bob",
+      username: "bob",
+      password: "123",
+      photo: "assets/image/empty.jpg"),
+  UserData(
+      name: "Alice",
+      username: "alice",
+      password: "123",
+      photo: "assets/image/peach.jpg"),
+  UserData(
+      name: "Joe",
+      username: "joe",
+      password: "123",
+      photo: "assets/image/dog9.jpg"),
+  UserData(
+      name: "Oreo",
+      username: "oreo",
+      password: "123",
+      photo: "assets/image/dog8.jpg"),
+  UserData(
+      name: "Bear",
+      username: "bear",
+      password: "123",
+      photo: "assets/image/dog7.jpg"),
+];
+
+List<Comment> _comment = [
+  Comment(user: _users[0], comment_info: "how are you", like_count: 19),
+  Comment(user: _users[1], comment_info: "how are you", like_count: 17),
+  Comment(user: _users[2], comment_info: "how are you", like_count: 10),
+  Comment(user: _users[3], comment_info: "how are you", like_count: 11),
+  Comment(user: _users[4], comment_info: "Cool!", like_count: 14),
+  Comment(user: _users[2], comment_info: "OMG! So Cute!!!", like_count: 1285),
+  Comment(
+      user: _users[3],
+      comment_info: "I totally agree with you!!",
+      like_count: 12),
+];
 
 List<Chat> chatList = [
   Chat(
@@ -63,8 +132,11 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   int _currentIndex = 0;
   final List<Widget> _pages = [
-    HomePage(title: "Map test"),
-    PlaceholderWidget(Colors.orange),
+    PlaceholderWidget(Colors.blue),
+    StoryPage(
+      Post_list: _posts,
+      Post_Index: 2,
+    ),
     ChatOverviewPage(chats: chatList),
     ProfilePage(person: demoUser1),
   ];
@@ -100,7 +172,7 @@ class _MainPageState extends State<MainPage> {
         onTap: _onItemTapped,
         items: [
           BottomNavigationBarItem(
-            icon: Icon(Icons.pin_drop_outlined),
+            icon: Icon(Icons.home_outlined),
             label: 'Home',
           ),
           BottomNavigationBarItem(
