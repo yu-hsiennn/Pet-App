eddbd9a13422bc14be523d23d61e8ecd240146d8
yu-hsiennn <fsps25896@gmail.com>
2023-05-09 19:21:40 +0800

    Implement Search location

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,7 +1,9 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'PostClass.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'locations.dart' as locations;
+import 'location_service.dart';
 
 
 class HomePage extends StatefulWidget {
@@ -15,6 +17,10 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final Map<String, Marker> _markers = {};
+  TextEditingController _searchController = TextEditingController();
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
   Future<void> _onMapCreated(GoogleMapController controller) async {
     final googleOffices = await locations.getGoogleOffices();
     setState(() {
@@ -33,22 +39,82 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  Future<void> _goToPlace(Map<String, dynamic> place) async {
+    final double lat = place['geometry']['location']['lat'];
+    final double lng = place['geometry']['location']['lng'];
+
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: LatLng(lat, lng), 
+          zoom: 16
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Google Office Locations'),
-          backgroundColor: Colors.green[700],
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: const CameraPosition(
-            target: LatLng(0, 0),
-            zoom: 2,
+    return Scaffold(
+      body: Stack(
+        children: [
+          Expanded(
+            child: GoogleMap(
+              myLocationButtonEnabled: false,
+              myLocationEnabled: true,
+              // onMapCreated: _onMapCreated,
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+              initialCameraPosition: const CameraPosition(
+                target: LatLng(22.99749, 120.22062),
+                zoom: 15,
+              ),
+              markers: _markers.values.toSet(),
+            ),
           ),
-          markers: _markers.values.toSet(),
-        ),
+          Positioned(
+            top: 10,
+            right: 15,
+            left: 15,
+            child: Container(
+              color: Colors.white,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      cursorColor: Colors.black,
+                      keyboardType: TextInputType.text,
+                      textInputAction: TextInputAction.go,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        contentPadding:
+                            EdgeInsets.symmetric(horizontal: 15),
+                        hintText: "Search Here"
+                      ),
+                      controller: _searchController,
+                      textCapitalization: TextCapitalization.words,
+                      onChanged: (value) {
+                        print(value);
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: IconButton(
+                      onPressed: () async {
+                        var place = await LocationService().getPlace(_searchController.text);
+                        _goToPlace(place);
+                      },
+                      icon: Icon(Icons.search),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
